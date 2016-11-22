277faafd0f232454f1a07371d449746567427f7a
CJHwong <cjhwong@gmail.com>
2016-11-22 15:00:25 +0800

    Optimized json loading logic

--- static/course/js/legacy.js
+++ static/course/js/legacy.js
@@ -31,12 +31,13 @@ var add_major = function(major, level) {
     $.each(course_of_majors[major], function(ik, iv) { //系上所有的選修課都先填入bulletin
         if (check_if_two_class(level).length == 1) { //代表只有一個班
             $.each(iv, function(jk, jv) {
-                $.each(courses[jv], function(kk, kv) {
+                $.each(courses[jv], function(_, kv) {
                     if (kv.obligatory_tf == false && kv.for_dept == major && kv.class == level) {
                         /************************************************************
                         kv.class == level limits only optional class for that grade will show!!!!
                         ************************************************************/
-                        check_which_bulletin(kv); //由fuction決定該貼到哪個年級的欄位
+                        // check_which_bulletin(kv); //由fuction決定該貼到哪個年級的欄位
+                        window.searchbar.addResult($(getCourseType(kv)), kv, window.timetable.language)
                     }
                 })
             })
@@ -44,10 +45,12 @@ var add_major = function(major, level) {
             var class_EN = level.split("")[1]; //班級的A或B，就是最後那個代碼
             if (ik.split("")[1] == class_EN) {
                 $.each(iv, function(jk, jv) {
-                    $.each(courses[jv], function(kk, kv) {
+                    $.each(courses[jv], function(_, kv) {
                         if (kv.obligatory_tf == false && kv.for_dept == major && kv.class.split("")[1] == class_EN && kv.class.split("")[0] == ik.split("")[0]) {
                             //console.log(kv);
-                            check_which_bulletin(kv); //由fuction決定該貼到哪個年級的欄位
+                            // check_which_bulletin(kv); //由fuction決定該貼到哪個年級的欄位
+                            console.log(kv)
+                            window.searchbar.addResult($(getCourseType(kv)), kv, window.timetable.language)
                             return false;
                         }
                     })
@@ -100,7 +103,9 @@ var show_optional_obligatory = function(course) {
     var trun_title = return_optional_obligatory_course_name(course);
     //cause the character at the end of title is truncate, so named it trun_title
     if (window.name_of_optional_obligatory[trun_title] > 1) {
-        bulletin_post($("#obligatory-post"), course, language);
+        // bulletin_post($("#obligatory-post"), course, language);
+        $('.stufinite-searchbar-placeholder').hide();
+        window.searchbar.addResult($(".obligatory-post"), course, window.timetable.language)
     }
 }
 
@@ -108,47 +113,4 @@ var show_optional_obligatory = function(course) {
 var check_if_two_class = function(level) { //為了讓我確認他是不是有分AB班，這個是用在選修課的填入判斷上
     level = level.split("");
     return (level); //可以從回傳的長度判斷是否有兩個班
-}
-
-
-/* ---------------------------------------------------------------- */
-var get_json_when_change_degree = function(path) {
-    $.when($.getJSON(path, function(json) { //getJSON會用function(X)傳回X的物件或陣列
-        //console.log(json);
-        $.each(json.course, function(ik, iv) {
-            if (typeof(window.course_of_majors[iv.for_dept]) == 'undefined') //如果這一列(列的名稱為索引值key)是空的也就是undefined，那就對他進行初始化，{}物件裡面可以放任意的東西，在下面會把很多陣列塞進這個物件裡面
-                window.course_of_majors[iv.for_dept] = {};
-            if (typeof(window.course_of_majors[iv.for_dept][iv.class]) == 'undefined') {
-                window.course_of_majors[iv.for_dept][iv.class] = []; //如果這一行(列的名稱為索引值key)是空的也就是undefined，那就對他進行初始化，[]裡面的是放陣列
-            }
-            window.course_of_majors[iv.for_dept][iv.class].push(iv.code); //把東西推進這個陣列裡，概念跟stack一樣
-            if (typeof(window.courses[iv.code]) == 'undefined') {
-                window.courses[iv.code] = [];
-            }
-            window.courses[iv.code].push(iv); //這邊可以直接把選課號當作索引值key，裡面的值為object
-            $.each(iv.time_parsed, function(jk, jv) { //建立日期的陣列
-                $.each(jv.time, function(mk, mv) {
-                    if (typeof(window.course_of_day[jv.day]) == 'undefined') {
-                        window.course_of_day[jv.day] = {};
-                    }
-                    if (typeof(window.course_of_day[jv.day][mv]) == 'undefined') {
-                        window.course_of_day[jv.day][mv] = [];
-                    }
-                    window.course_of_day[jv.day][mv].push(iv);
-                })
-            })
-            if (typeof(window.teacher_course[iv.professor]) == 'undefined') { //建立老師名稱的陣列
-                window.teacher_course[iv.professor] = [];
-            }
-            window.teacher_course[iv.professor].push(iv);
-            if (typeof(window.name_of_course[iv.title_parsed.zh_TW]) == 'undefined') { //中文課名陣列
-                window.name_of_course[iv.title_parsed.zh_TW] = [];
-            }
-            window.name_of_course[iv.title_parsed.zh_TW].push(iv);
-            if (typeof(window.name_of_course[iv.title_parsed.en_US]) == 'undefined') { //英文課名陣列
-                window.name_of_course[iv.title_parsed.en_US] = [];
-            }
-            window.name_of_course[iv.title_parsed.en_US].push(iv);
-        });
-    }))
 }
--- static/course/js/main.js
+++ static/course/js/main.js
@@ -17,6 +17,78 @@
     });
 })()
 
+function buildCourseIndex(json) {
+    $.each(json.course, function(_, iv) {
+        //初始化 course_of_majors
+        if (typeof(window.course_of_majors[iv.for_dept]) == 'undefined') {
+            window.course_of_majors[iv.for_dept] = {};
+        }
+        if (typeof(window.course_of_majors[iv.for_dept][iv.class]) == 'undefined') {
+            window.course_of_majors[iv.for_dept][iv.class] = [];
+        }
+        window.course_of_majors[iv.for_dept][iv.class].push(iv.code);
+
+        //初始化 courses
+        if (typeof(window.courses[iv.code]) == 'undefined') {
+            window.courses[iv.code] = [];
+        }
+        window.courses[iv.code].push(iv);
+
+        //初始化 course_of_day
+        $.each(iv.time_parsed, function(_, jv) {
+            $.each(jv.time, function(_, mv) {
+                if (typeof(window.course_of_day[jv.day]) == 'undefined') {
+                    window.course_of_day[jv.day] = {};
+                }
+                if (typeof(window.course_of_day[jv.day][mv]) == 'undefined') {
+                    window.course_of_day[jv.day][mv] = [];
+                }
+                window.course_of_day[jv.day][mv].push(iv);
+            })
+        })
+
+        //初始化 teacher_course
+        if (typeof(window.teacher_course[iv.professor]) == 'undefined') {
+            window.teacher_course[iv.professor] = [];
+        }
+        window.teacher_course[iv.professor].push(iv);
+
+        //初始化 name_of_course
+        if (typeof(window.name_of_course[iv.title_parsed.zh_TW]) == 'undefined') {
+            //中文課名陣列
+            window.name_of_course[iv.title_parsed.zh_TW] = [];
+        }
+        window.name_of_course[iv.title_parsed.zh_TW].push(iv);
+        if (typeof(window.name_of_course[iv.title_parsed.en_US]) == 'undefined') {
+            //英文課名陣列
+            window.name_of_course[iv.title_parsed.en_US] = [];
+        }
+        window.name_of_course[iv.title_parsed.en_US].push(iv);
+    });
+}
+
+function buildDeptArray(depJson) {
+    $.each(depJson, function(_, iv) {
+        if (typeof(window.department_name[iv.degree]) == 'undefined') {
+            window.department_name[iv.degree] = {};
+        }
+
+        $.each(iv.department, function(_, jv) {
+            if (typeof(window.department_name[iv.degree][jv.zh_TW]) == 'undefined') {
+                window.department_name[iv.degree][jv.zh_TW] = {};
+            }
+
+            var option = "";
+            option += jv.value + '-' + jv["zh_TW"];
+            window.department_name[iv.degree][jv.zh_TW]["zh_TW"] = option;
+
+            var option = "";
+            option += jv.value + '-' + jv["en_US"];
+            window.department_name[iv.degree][jv.zh_TW]["en_US"] = option;
+        })
+    })
+};
+
 function legacyInit() {
     window.user = {
         "user_name": "",
@@ -40,44 +112,14 @@ function legacyInit() {
     window.course_of_majors = {}; //以科系和年級為 key 的二維物件
 
     window.name_of_optional_obligatory = [] //這是用來存系上的必修課，檢查有沒有課名是重複的，若有就讓使用者自行決定要上哪堂
-    window.url_base = ""; //used to be the url that link to the syllabus of that course.
-    window.haveloadin = {
-        D: false,
-        G: false,
-        N: false,
-        O: false,
-        U: false,
-        W: false
-    }; //used to checked whether that json of specific degree has been loaded in or not, if it did, the value turn to ture.
-    window.lastupdatetime = ""; //show the update time on server.
 
     //1. O.json is suitable for all kind of degree, so it will be loaded in automatically.
     //2. 當文件準備好的時候，讀入department的json檔, 因為這是顯示系所，沒多大就全部都載進來
-    var build_department_arr = function(depJson) {
-        $.each(depJson, function(_, iv) {
-            if (typeof(window.department_name[iv.degree]) == 'undefined') {
-                window.department_name[iv.degree] = {};
-            }
-
-            $.each(iv.department, function(_, jv) {
-                if (typeof(window.department_name[iv.degree][jv.zh_TW]) == 'undefined') {
-                    window.department_name[iv.degree][jv.zh_TW] = {};
-                }
-
-                var option = "";
-                option += jv.value + '-' + jv["zh_TW"];
-                window.department_name[iv.degree][jv.zh_TW]["zh_TW"] = option;
-
-                var option = "";
-                option += jv.value + '-' + jv["en_US"];
-                window.department_name[iv.degree][jv.zh_TW]["en_US"] = option;
-            })
+    $.when($.getJSON("/static/course/json/O.json", buildCourseIndex),
+        $.getJSON("/static/course/json/department.json", buildDeptArray)
+    )
+    $.when($.getJSON("/static/course/json/U.json", buildCourseIndex))
+        .then(function() {
+            add_major("資訊科學與工程學系學士班", "2");
         })
-    };
-    $.when(get_json_when_change_degree("/static/course/json/U.json"),
-        get_json_when_change_degree("/static/course/json/O.json"),
-        $.getJSON("/static/course/json/department.json", build_department_arr)
-    ).then(function() {
-        add_major("資訊科學與工程學系學士班", "1");
-    })
 }
