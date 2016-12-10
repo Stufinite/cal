0f5febf30830f117416c7a577590b26727a9e8a6
CJHwong <cjhwong@gmail.com>
2016-12-10 20:06:31 +0800

    Fixed user grade assignment

--- cal/timetable/views/views.py
+++ cal/timetable/views/views.py
@@ -11,15 +11,15 @@ def init_user(request):
     if not request.session.session_key:
         request.session.save()
     try:
-        # User.get(request.session.session_key)
-        User.get_test()
+        User.get(request.session.session_key)
+        # User.get_test()
     except HTTPError:
         return HttpResponseRedirect('https://login.stufinite.faith')
 
     u = {
         'is_authenticated': True,
         'username': User.username,
-        'grade': 2,
+        'grade': User.grade,
         'major': User.major,
         'second_major': User.second_major,
         'career': User.career,
