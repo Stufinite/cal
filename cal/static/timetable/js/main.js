9d26948d58cd348afe3e58f463396c811f619a23
CJHwong <cjhwong@gmail.com>
2016-12-19 22:16:50 +0800

    Finished search function

--- cal/static/timetable/js/main.js
+++ cal/static/timetable/js/main.js
@@ -9,21 +9,6 @@
             searchbar.show();
         });
 
-        // Initialize search-form behavior
-        document.querySelector("#search-form").addEventListener("input", (e) => {
-            let key = $(e.target).val();
-            $.getJSON("/search/?keyword=" + key + "&school=NCHU", (c_by_key) => {
-                for (let i of c_by_key) {
-                    $.getJSON("/api/get/course/" + i.DBid, (c_by_id) => {
-                        for (let c_by_code of window.timetable.getCourse('code', c_by_id[0].code)) {
-                            window.searchbar.clear()
-                            window.searchbar.addResult($(window.timetable.getCourseType(c_by_code)), c_by_code, window.timetable.language)
-                        }
-                    });
-                }
-            });
-        });
-
         document.querySelector(".stufinite-app-searchbar-toggle").addEventListener("click", (e) => {
             if (window.searchbar.isVisible) {
                 window.searchbar.hide();
