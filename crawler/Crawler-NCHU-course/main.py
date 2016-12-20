94176519849faac4eb55e12d4f1599d4d7336c42
davidtnfsh <davidtnfsh@gmail.com>
2016-12-20 20:08:24 +0800

    start the whole action of Crawler and import2DB with main.py

--- crawler/Crawler-NCHU-course/main.py
+++ crawler/Crawler-NCHU-course/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from crawler import Crawler
+from import2DB import import2Mongo
 if __name__ == "__main__":
 	c = Crawler()
 	c.start()
+	i = import2Mongo()
+	i.save2DB()
