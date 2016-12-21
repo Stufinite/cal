5425bb4f51e2e28ab9e470b28b95e7fb3126420f
davidtnfsh <davidtnfsh@gmail.com>
2016-12-21 15:49:23 +0800

    將各學制各系所的必選修課程代碼存進mongo

--- crawler/Crawler-NCHU-course/import2DB.py
+++ crawler/Crawler-NCHU-course/import2DB.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import json
+from pymongo import MongoClient
 class import2Mongo(object):
 	"""docstring for import2Mongo"""
 	def __init__(self, uri=None):
-		from pymongo import MongoClient
 		self.JSONdir = 'json'
-		self.degree = ['U', 'G', 'D', 'N', 'O', 'W']
+		self.degree2Chi = {"U":"學士班", "G":"碩士班","D":"博士班","N":"進修學士班","W":"碩專班"}
 		self.deptSet = set()
 		self.client = MongoClient(uri)
 		self.db = self.client['timetable']
 		self.Collect = self.db['CourseOfDept']
+		self.chgTable = json.load(open('fallback/json/department.json', 'r'))
 		
 	def parseJson(self, degree):
-		import json
 		def getJson(degree):
 			with open(self.JSONdir+'/'+degree+'.json', 'r', encoding='utf8') as f:
 				return json.load(f)
@@ -27,10 +28,19 @@ class import2Mongo(object):
 				return 'obligatory'
 			return 'optional'
 
+		def getDeptCode(degree, deptName):
+			for i in self.chgTable:
+				if i['degree'] == self.degree2Chi[degree]:
+					for j in i['department']:
+						if j['name'] == deptName or deptName in j['name']:
+							return j['value']
+			print(degree, deptName)
+			raise Exception("fuck")
+
 		result = {}
 		jsonDict = getJson(degree)
 		for i in jsonDict['course']:
-			dept = i['for_dept']
+			dept = getDeptCode(degree, i['for_dept'])
 			code = i['code']
 			grade = i['class']
 			className, grade = getClass(grade)
@@ -49,7 +59,7 @@ class import2Mongo(object):
 		return result
 
 	def save2DB(self):
-		for degree in self.degree:
+		for degree in self.degree2Chi:
 			#這邊需要修改，因為學校沒有完全按照學至分類乾淨，所以才需要每次都把set清空####
 			self.deptSet = set()
 			#############################
