970f9cfb963296212af267203c7ba6febf08122d
davidtnfsh <davidtnfsh@gmail.com>
2016-12-21 17:35:02 +0800

    finish CourseOfDept api

---
+++ cal/course/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from django.http import JsonResponse
+from djangoApiDec.djangoApiDec import queryString_required
+from pymongo import MongoClient
+
+class Course(object):
+	"""docstring for Course"""
+	def __init__(self, dept, uri=None):
+		self.dept = dept
+		self.collect = MongoClient(uri)['timetable']['CourseOfDept']
+
+	def Cursor2Dict(self, cursor):
+		return list(cursor)[0]
+
+	def getByDept(self):
+		CourseDict = self.Cursor2Dict(self.collect.find({self.dept:{"$exists":True}}).limit(1))
+		return CourseDict[self.dept]
+
+@queryString_required(['dept'])
+def CourseOfDept(request):
+	"""
+		Generate list of obligatory and optional course of specific Dept.
+	"""
+	dept = request.GET['dept']
+	c = Course(dept)
+	return JsonResponse(c.getByDept(), safe=False)
