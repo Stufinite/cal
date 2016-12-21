6c1c0c43247a57bfea21ba09153249d0d3e4690b
davidtnfsh <davidtnfsh@gmail.com>
2016-12-21 10:24:07 +0800

    將學校產出的api裏面有把錯誤學至分類到錯誤的json的錯誤課程重新糾正，博士班的課不會出現再碩士班了

--- crawler/Crawler-NCHU-course/crawler.py
+++ crawler/Crawler-NCHU-course/crawler.py
@@ -6,6 +6,14 @@ class Crawler(object):
 	def __init__(self):
 		self.degree = ['U', 'G', 'D', 'N', 'O', 'W']
 		self.url = 'https://onepiece.nchu.edu.tw/cofsys/plsql/json_for_course?p_career='
+		self.errCourse = {
+			"U":[],
+			"G":[],
+			"D":[],
+			"N":[],
+			"O":[],
+			"W":[]
+		}
 		try:
 			subprocess.call(['rm', '-rf', 'json'])
 			subprocess.call(['mkdir', 'json'])
@@ -19,14 +27,42 @@ class Crawler(object):
 				tmp+=i
 		return tmp
 
-	def validateTmpJson(self, tmpFile):
+	def validateTmpJson(self, tmpFile, degree):
 		# truncate invalid char to turn it into json
 		jsonStr = ""
 		with open('json/'+tmpFile, 'r', encoding='UTF-8') as f:
 			for line in f:
 				tmp = self.truncateNewLineSpace(line)
 				jsonStr +=tmp
+		jsonStr = self.checkDegree(jsonStr, degree)
 		return jsonStr
+
+	def checkDegree(self, jsonStr, degree):
+		# correct those Course which were placed in wrong degree dict.
+		jsonDict = json.loads(jsonStr)
+		degreeTable = {"U":["1","2","3","4","5","1A","1B","2A","2B","3A","3B", "4A", "4B", "5A", "5B"],"G":["6", "7"], "D":["8", "9"], "N":["1","2","3","4","5"],"O":["0","1","2","3","4","5"], "W":["6", "7"]}
+		for index, value in enumerate(jsonDict['course']):
+			if value['class'] not in degreeTable[degree]:
+				jsonDict['course'].remove(value)
+				if value['class'] in degreeTable['D']:
+					self.errCourse['D'].append(value)
+				elif '在職' in value['for_dept']:
+					self.errCourse['W'].append(value)
+				elif '碩士' in value['for_dept'] :
+					self.errCourse['G'].append(value)
+				elif '進修' in value['for_dept']:
+					self.errCourse['N'].append(value)
+				elif value['class'] in degreeTable['U']:
+					self.errCourse['U'].append(value)
+				elif value['class'] == 0 or value['for_dept'].find('全校共同')!=-1:
+					self.errCourse['O'].append(value)
+				elif '研究所' in value['for_dept']:
+					self.errCourse['G'].append(value)
+				else:
+					print(value)
+					raise Exception('fuck')
+		return json.dumps(jsonDict)
+
 		
 	def start(self):
 		for d in self.degree:
@@ -41,13 +77,14 @@ class Crawler(object):
 					json_str = json.dumps(re.json(), ensure_ascii=False, sort_keys=True)
 					#re.json() will check whether 're' is type of json
 					#json.dumps will return string.
+					json_str = self.checkDegree(json_str, d)
 					f.write(json_str)#f.write only accept and write string into files.
 			except Exception as e:
 				tmpFile = d + '_tmp.json'
 				with open('json/'+tmpFile, 'w', encoding='UTF-8') as f:
 					f.write(re.text)
 
-				jsonStr = self.validateTmpJson(tmpFile)
+				jsonStr = self.validateTmpJson(tmpFile, d)
 				try:
 					testJsonvalid=json.loads(jsonStr)
 					formalFile = d + '.json'
@@ -57,4 +94,10 @@ class Crawler(object):
 					print(e)    
 					raise e    
 
-
+		for key, value in self.errCourse.items():
+			with open("json/"+key+".json", 'r', encoding='utf-8') as f:
+				tmp = json.load(f)
+				for i in self.errCourse[key]:
+					tmp['course'].append(i)
+			with open("json/"+key+".json", 'w', encoding='utf-8') as f:
+				json.dump(tmp, f)
