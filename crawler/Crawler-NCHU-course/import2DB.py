f15cdf3bde948f66be18d8e98016ed9ee3b8b27e
davidtnfsh <davidtnfsh@gmail.com>
2017-01-14 02:38:14 +0800

    [Maybe fix] try to re-construct CourseOfDept

--- crawler/Crawler-NCHU-course/import2DB.py
+++ crawler/Crawler-NCHU-course/import2DB.py
@@ -12,14 +12,14 @@ class import2Mongo(object):
 		self.db = self.client['timetable']
 		self.DeptCollect = self.db['CourseOfDept']
 		self.CourseOfTime = self.db['CourseOfTime']
-		self.chgTable = json.load(open('fallback/json/department.json', 'r'))
+		self.chgTable = dict(tuple(tuple(dept['name'], dept['value'])for degree in json.load(open('fallback/json/department.json', 'r')) for dept in degree['department']))
 
 	def AddHeader(self, document, degree):
 		document['degree'] = degree
 		document['school'] = "NCHU"
 		return document
 
-	def BuildByDept(self, degree, jsonDict):
+	def BuildByDept(self, jsonDict):
 		def getClass(grade):
 			if len(grade) == 1:
 				return 'ClassA', grade
@@ -32,21 +32,22 @@ class import2Mongo(object):
 				return 'obligatory'
 			return 'optional'
 
-		def getDeptCode(degree, deptName, grade):
+		def getDeptCode(deptName, grade):
 			if len(grade) > 1:
 				deptName = deptName + ' ' + grade[-1].upper()
 
-			for i in self.chgTable:
-				if i['degree'] == self.degree2Chi[degree]:
-					for j in i['department']:
-						if j['name'] == deptName or deptName in j['name']:
-							return j['value']
-			print(degree, deptName)
-			return False
+			return self.chgTable.get(deptName, False)
+			# for i in self.chgTable:
+			# 	if i['degree'] == self.degree2Chi[degree]:
+			# 		for j in i['department']:
+			# 			if j['name'] == deptName or deptName in j['name']:
+			# 				return j['value']
+			# print(degree, deptName)
+			# return False
 
 		result = {}
-		for i in jsonDict['course']:
-			dept = getDeptCode(degree, i['for_dept'], i['class'])
+		for i in jsonDict:
+			dept = getDeptCode(i['for_dept'], i['class'])
 			# dept == False means getDeptCode has error
 			if dept == False: continue
 			code = i['code']
@@ -54,17 +55,25 @@ class import2Mongo(object):
 			className, grade = getClass(grade)
 			obligat = i['obligatory_tf']
 			oblAttr = getObliAttr(obligat)
-			if dept not in self.deptSet:
-				result[dept]={
-						'obligatory':{},
-						'optional':{}
-					}
-				self.deptSet.add(dept)
+
+			result.setdefault(dept, 
+				{
+					'obligatory':{},
+					'optional':{}
+				}
+			)
+			# if dept not in self.deptSet:
+			# 	result[dept]={
+			# 			'obligatory':{},
+			# 			'optional':{}
+			# 		}
+			# 	self.deptSet.add(dept)
 			result[dept][oblAttr].setdefault(grade, []).append(code)
 
-		return result
+		resultList = tuple( dict(dept=dept, course=course, school='NCHU') for dept, course in result.items())
+		self.DeptCollect.insert(resultList)
 
-	def BuildByTime(self, degree, jsonDict):
+	def BuildByTime(self, jsonDict):
 		result = {
 			"1":{"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[],"10":[],"11":[],"12":[],"13":[]},
 			"2":{"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[],"10":[],"11":[],"12":[],"13":[]},
@@ -84,21 +93,22 @@ class import2Mongo(object):
 
 	def save2DB(self):
 		def getJson(degree):
-			with open(self.JSONdir+'/'+degree+'.json', 'r', encoding='utf8') as f:
-				return json.load(f)
+			return json.load(open(self.JSONdir+'/'+degree+'.json', 'r', encoding='utf8'))
 
 		self.DeptCollect.remove({})
 		self.CourseOfTime.remove({})
-		for degree in self.degree2Chi:
-			jsonDict = getJson(degree)
-			# #這邊需要修改，因為學校沒有完全按照學至分類乾淨，所以才需要每次都把set清空####
-			# self.deptSet = set()
-			# #############################
-			if degree != "O":
-				deptDoc = self.BuildByDept(degree, jsonDict)
-				deptDoc = self.AddHeader(deptDoc, degree)
-				self.DeptCollect.update({ "$and":[{"school":"NCHU"}, {'degree':degree}] }, deptDoc, upsert=True)
 
-			timeDoc = self.BuildByTime(degree, jsonDict)
-			timeDoc = self.AddHeader(timeDoc, degree)
-			self.CourseOfTime.update({ "$and":[{"school":"NCHU"}, {'degree':degree}] }, timeDoc, upsert=True)
+		AllJson = tuple()
+		for degree in self.degree2Chi:
+			AllJson += tuple(_ for _ in getJson(degree)['course'])
+
+		self.BuildByDept(AllJson)
+		self.BuildByTime(jsonDict)
+		# if degree != "O":
+		# 	deptDoc = self.BuildByDept(degree, jsonDict)
+		# 	deptDoc = self.AddHeader(deptDoc, degree)
+		# 	self.DeptCollect.update({ "$and":[{"school":"NCHU"}, {'degree':degree}] }, deptDoc, upsert=True)
+
+		# timeDoc = self.BuildByTime(degree, jsonDict)
+		# timeDoc = self.AddHeader(timeDoc, degree)
+		# self.CourseOfTime.update({ "$and":[{"school":"NCHU"}, {'degree':degree}] }, timeDoc, upsert=True)
