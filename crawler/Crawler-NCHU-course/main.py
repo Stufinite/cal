73b3c1cb9b16621deab239c2b847f583e82c8314
davidtnfsh <davidtnfsh@gmail.com>
2017-01-18 00:30:18 +0800

    auto run crawler half an hour

--- crawler/Crawler-NCHU-course/main.py
+++ crawler/Crawler-NCHU-course/main.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 from crawler import Crawler
 from import2DB import import2Mongo
+import time
 if __name__ == "__main__":
-	# c = Crawler()
-	# c.start()
-	i = import2Mongo()
-	i.save2DB()
+	while True:		
+		c = Crawler()
+		c.start()
+		i = import2Mongo()
+		i.save2DB()
+		time.sleep(1800)
