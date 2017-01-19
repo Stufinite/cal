665dc8b55dcf022d3dbc1c38dfb5f451834ee1fb
davidtnfsh <davidtnfsh@gmail.com>
2017-01-19 11:26:08 +0800

    use bulk_create to optimize build_course, prevent it to be time consuming

--- cal/timetable/views/dev.py
+++ cal/timetable/views/dev.py
@@ -32,43 +32,50 @@ def build_course(school: str, semester: str):
     import json
     onlycourse = [x for x in listdir(settings.STATICFILES_DIRS[
                                      0] + '/timetable/json/{}/{}'.format(school.upper(), str(semester)))]
+    CourseList = []
+    CodeSet = set()
+
     for filename in onlycourse:
-        with open(settings.STATICFILES_DIRS[0] + '/timetable/json/{}/{}/{}'.format(school.upper(), str(semester), filename), 'r') as f:
-            data = json.loads(f.read())
-            for c in data["course"]:
-                try:
-                    print(filename, c['code'], c['title'], c['professor'])
+        data = json.load(open(settings.STATICFILES_DIRS[0] + '/timetable/json/{}/{}/{}'.format(school.upper(), str(semester), filename), 'r'))
+        for c in data["course"]:
+            try:
+                print(filename, c['code'], c['title'], c['professor'])
 
-                    time = ''
-                    for i in c['time_parsed']:
-                        time += str(i['day']) + '-'
-                        for j in i['time']:
-                            time += str(j) + '-'
-                        time = time[:-1]
-                        time += ','
+                time = ''
+                for i in c['time_parsed']:
+                    time += str(i['day']) + '-'
+                    for j in i['time']:
+                        time += str(j) + '-'
+                    time = time[:-1]
+                    time += ','
 
-                    d, created = Course.objects.get_or_create(
-                        school=school.upper(),
-                        semester=str(semester),
-                        code=c['code'],
-                        credits=c['credits'],
-                        title='{},{}'.format(
-                            c['title_parsed']['zh_TW'],
-                            c['title_parsed']['en_US']
-                        ),
-                        department=c['department'],
-                        professor=c['professor'],
-                        time=time[:-1],
-                        intern_location=c['intern_location'][0],
-                        location=c['location'][0],
-                        obligatory=c['obligatory_tf'],
-                        language=c['language'],
-                        prerequisite=c['prerequisite'],
-                        note=c['note'],
-                        discipline=c['discipline'],
+                if c['code'] not in CodeSet:
+                    CodeSet.add(c['code'])
+
+                    CourseList.append( 
+                        Course(
+                            school=school.upper(),
+                            semester=str(semester),
+                            code=c['code'],
+                            credits=c['credits'],
+                            title='{},{}'.format(
+                                c['title_parsed']['zh_TW'],
+                                c['title_parsed']['en_US']
+                            ),
+                            department=c['department'],
+                            professor=c['professor'],
+                            time=time[:-1],
+                            intern_location=c['intern_location'][0],
+                            location=c['location'][0],
+                            obligatory=c['obligatory_tf'],
+                            language=c['language'],
+                            prerequisite=c['prerequisite'],
+                            note=c['note'],
+                            discipline=c['discipline'],
+                        )
                     )
-                    if not created:
-                        d.save()
-                except:
-                    pass
+            except Exception as e:
+                print(e)
+
+    Course.objects.bulk_create(CourseList)
     return JsonResponse({"state": "ok"})
